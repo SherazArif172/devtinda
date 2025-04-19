42b2976e0f1bdca45f614b2ffea52a9223b42461
Sheraz Arif <sherazarifofficial@gmail.com>
2025-04-19 15:42:15 +0500

    handeling the error

--- devTinderBackend/src/app.js
+++ devTinderBackend/src/app.js
@@ -3,14 +3,21 @@ const express = require("express");
 const app = express();
 const { adminAuth, userAuth } = require("./middlewares/auth.middleware.js");
 
-app.use("/admin", adminAuth);
-app.use("/user", userAuth);
+// app.use("/admin", adminAuth);
+// app.use("/user", userAuth);
 
-app.get("/user/data", (req, res, next) => {
-  res.send("user data ");
-});
-app.get("/admin/data", (req, res, next) => {
-  res.send("admin data ");
+// app.get("/user/data", (req, res, next) => {
+//   res.send("user data ");
+// });
+// app.get("/admin/data", (req, res, next) => {
+//   res.send("admin data ");
+// });
+app.get("/user", (req, res) => {
+  try {
+    throw new Error("user not found");
+  } catch (error) {
+    res.send("semthing went wrong");
+  }
 });
 
 app.listen(3000, () => {
