f9d0e6a48a4976b94ec5d87b6a45186e7df43686
Sheraz Arif <sherazarifofficial@gmail.com>
2025-05-02 05:58:14 +0500

    add the browser router setup

--- devTindaFrontend/src/App.jsx
+++ devTindaFrontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./_components/Navbar";
+import Body from "./_components/Body";
 
 function App() {
   return (
     <>
-      <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<p>jkhsdf</p>} />
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<p>asdfasdf</p>} />
+            <Route path="/signup" element={<p>jnsdkjfnksnbdfkfnb</p>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
